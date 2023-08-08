d7f4fb49a1bca18e0b5ca681b37105b01130fd03
Nolan Guyon <nolan.guyon@inetum.com>
2023-08-08 15:11:41 +0200

    Fix only div received

--- UMI3D-Samples/Assets/UMI3D SDK/EnvironmentDevelopmentKit/Collaboration/Runtime/WorldController/StandAlone/UMI3DStandAloneApi.cs
+++ UMI3D-Samples/Assets/UMI3D SDK/EnvironmentDevelopmentKit/Collaboration/Runtime/WorldController/StandAlone/UMI3DStandAloneApi.cs
@@ -103,7 +103,7 @@ namespace umi3d.worldController
                 HttpListenerResponse res = e.Response;
                 var form = result as Form;
                 Debug.Log(result.ToJson(Newtonsoft.Json.TypeNameHandling.None));
-                res.WriteContent(System.Text.Encoding.UTF8.GetBytes(result.ToJson(Newtonsoft.Json.TypeNameHandling.None)));
+                res.WriteContent(System.Text.Encoding.UTF8.GetBytes(result.ToJson(Newtonsoft.Json.TypeNameHandling.All)));
             }
         }
 
