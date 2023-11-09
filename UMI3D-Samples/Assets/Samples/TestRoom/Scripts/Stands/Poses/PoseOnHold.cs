a8af07f3a630e07fc639b58146095fec9477ecfa
Charles Marichal <charles-eole.marichal@inetum.com>
2023-11-09 15:45:37 +0100

    Add pose on interaction behaviours

---
+++ UMI3D-Samples/Assets/Samples/TestRoom/Scripts/Stands/Poses/PoseOnHold.cs
@@ -0,0 +1,44 @@
+﻿using inetum.unityUtils;
+
+using umi3d.common.userCapture.pose;
+using umi3d.edk;
+using umi3d.edk.interaction;
+using umi3d.edk.userCapture.pose;
+
+using UnityEngine;
+
+public class PoseOnHold : MonoBehaviour
+{
+    private UMI3DPoseAnimator poseAnimator;
+    private UMI3DEvent umi3dEvent;
+    private UMI3DEnvironmentPoseCondition poseCondition;
+
+    private void Start()
+    {
+        poseAnimator = gameObject.GetOrAddComponent<UMI3DPoseAnimator>();
+        umi3dEvent = gameObject.GetOrAddComponent<UMI3DEvent>();
+
+        poseCondition = new UMI3DEnvironmentPoseCondition();
+        poseAnimator.environmentPoseConditions.Add(poseCondition);
+
+        umi3dEvent.onHold.AddListener((content) => RequestPoseApplication(content.user));
+        umi3dEvent.onRelease.AddListener((content) => RequestPoseStop(content.user));
+    }
+
+    private void RequestPoseApplication(UMI3DUser user)
+    {
+        TryActivatePoseAnimatorRequest activatePoseAnimatorRequest = new(poseAnimator.Id()) { users = new() { user } };
+
+        Transaction t = new(true);
+        t.AddIfNotNull(poseCondition.Validate(user));
+        t.AddIfNotNull(activatePoseAnimatorRequest);
+        t.Dispatch();
+    }
+
+    private void RequestPoseStop(UMI3DUser user)
+    {
+        Transaction t = new(true);
+        t.AddIfNotNull(poseCondition.Invalidate(user));
+        t.Dispatch();
+    }
+}
